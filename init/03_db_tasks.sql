720c5b7f5245736f95a8e74f3df890e1f7610066
Juan Fran Martin <juanframaro@gmail.com>
2021-11-13 16:57:37 +0000

    fixed database errors

--- init/03_db_tasks.sql
+++ init/03_db_tasks.sql
@@ -1,12 +1,7 @@
 GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'%' IDENTIFIED BY 'test';
-USE pickauto_dn;
+USE pickauto;
 
-CREATE TABLE IF NOT EXISTS rol (
-    id_rol INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    rol VARCHAR(10) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS User (
     DNI INT UNSIGNED NOT NULL PRIMARY KEY,
     password_key VARCHAR(20) NOT NULL,
     email VARCHAR(50) NOT NULL,
@@ -15,43 +10,48 @@ CREATE TABLE IF NOT EXISTS user (
     last_name VARCHAR(40) NOT NULL,
     phone_number INT UNSIGNED,
     birth_date DATE NOT NULL,
-    rol ENUM VARCHAR(10),
-    profile_image VARCHAR(100),
+    profile_image VARCHAR(100)
     /*Las imágenes van en una carpeta y en la tabla se hace referencia a la ruta*/
-    FOREIGN KEY (rol) REFERENCES rol(id_rol)
 );
 
-CREATE TABLE IF NOT EXISTS id_picker (
+CREATE TABLE IF NOT EXISTS Rol (
+    id_rol INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_user INT UNSIGNED NOT NULL,
+    rol VARCHAR(10) NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES User(DNI)
+);
+
+CREATE TABLE IF NOT EXISTS Picker (
     id_picker INT UNSIGNED NOT NULL PRIMARY KEY,
     available BOOLEAN NOT NULL,
     start_time TIME,
     finish_time TIME,
     rating TINYINT DEFAULT 5,
-    FOREIGN KEY (id_picker) REFERENCES user(DNI)
+    FOREIGN KEY (id_picker) REFERENCES User(DNI)
 );
 
-CREATE TABLE IF NOT EXISTS vehicles (
+CREATE TABLE IF NOT EXISTS Vehicle (
     plate_number VARCHAR(8) NOT NULL PRIMARY KEY,
     id_owner INT UNSIGNED NOT NULL,
     brand VARCHAR(20),
     model VARCHAR(20),
-    power INT UNSIGNED,
+    powered INT UNSIGNED,
     kilometers INT UNSIGNED,
-    combustible ENUM('diesel', 'gasolina', 'electrico', 'gas'),
+    fuel ENUM('diesel', 'gasolina','híbrido', 'electrico', 'gas'),
     vehicle_description VARCHAR(200),
     vehicle_image VARCHAR(100),
-    FOREIGN KEY (id_owner) REFERENCES id_owner(id_owner)
+    FOREIGN KEY (id_owner) REFERENCES User(DNI)
 );
 
-CREATE TABLE IF NOT EXISTS services (
+CREATE TABLE IF NOT EXISTS Services (
     id_service INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    service_type ENUM('A', 'B', 'C'),
+    service_type ENUM('Chapa y Pintura', 'Electricidad', 'Mantenimiento', 'Transporte del Vehículo', 'Limpieza', 'ITV'),
     service_description VARCHAR(200)
 );
 
-CREATE TABLE IF NOT EXISTS appointment (
+CREATE TABLE IF NOT EXISTS Appointment (
     id_appointment INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_vehicle INT UNSIGNED NOT NULL,
+    id_vehicle VARCHAR(8) NOT NULL,
     id_service INT UNSIGNED NOT NULL,
     id_picker INT UNSIGNED NOT NULL,
     pick_up_place VARCHAR(100) NOT NULL,
@@ -60,15 +60,15 @@ CREATE TABLE IF NOT EXISTS appointment (
     notes VARCHAR(200),
     delivery_place VARCHAR(100) NOT NULL,
     garage VARCHAR(100),
-    CONSTRAINT FOREIGN KEY (id_vehicle) REFERENCES vehicles(plate_number),
-    CONSTRAINT FOREIGN KEY (id_service) REFERENCES services(id_service),
-    CONSTRAINT FOREIGN KEY (id_picker) REFERENCES id_picker(id_picker)
-)ENGINE = INNODB;
+    CONSTRAINT FOREIGN KEY (id_vehicle) REFERENCES Vehicle(plate_number),
+    CONSTRAINT FOREIGN KEY (id_service) REFERENCES Services(id_service),
+    CONSTRAINT FOREIGN KEY (id_picker) REFERENCES Picker(id_picker)
+);
 
-CREATE TABLE IF NOT EXISTS rating (
+CREATE TABLE IF NOT EXISTS Rating (
     id_rating INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_appointment INT UNSIGNED NOT NULL,
     notes VARCHAR(200),
     rating TINYINT UNSIGNED,
-    FOREIGN KEY (id_appointment) REFERENCES appointment(id_appointment)
+    CONSTRAINT FOREIGN KEY (id_appointment) REFERENCES Appointment(id_appointment)
 );
