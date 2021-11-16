23c0a7df6e440dc6d6b8a3b06968392b28fe3bfa
Marta Ramiro <martaramiroperez@gmail.com>
2021-11-16 20:35:50 +0100

    add data to tables

---
+++ init/05_data_insertion.sql
@@ -0,0 +1,13 @@
+USE pickauto;
+
+INSERT INTO Rol (rol) VALUES ('Owner'), ('Picker');
+
+CALL Insert_user(12345678, 'seguridad', 'sawyer@lost.org' , 'Sevilla', 'James', 'Ford', 1, 123456789, '1969-07-20', 'Sawyer');
+CALL Insert_user(12345679, 'seguridad', 'jackshephard@lost.org' , 'Barcelona', 'Jack', 'Shephard', 1, 123456389, '1966-07-14', 'Doctor');
+CALL Insert_user(12345678, 'seguridad', 'kateausten@lost.org' , 'Sevilla', 'Kate', 'Hudson', 1, 123356789, '1979-08-03', 'Kate');
+CALL Insert_user(12345678, 'seguridad', 'johnlocke@lost.org' , 'Madrid', 'John', 'Locke', 2, 123356289, '1952-07-15', 'Locke');
+CALL Insert_user(12345678, 'seguridad', 'Hurley@lost.org' , 'Bilbao', 'Hugo', 'Reyes', 1, 123356729, '1973-04-28', 'Hurley');
+CALL Insert_user(12345678, 'seguridad', 'Charliepace@lost.org' , 'Bilbao', 'Charlie', 'Pace', 2, 113356729, '1976-12-08', 'Charlie');
+CALL Insert_user(12345678, 'seguridad', 'BooneCarlyle@lost.org' , 'Barcelona', 'Boone', 'Carlyle', 1, 123356029, '1978-12-08', 'Boone');
+CALL Insert_user(12345678, 'seguridad', 'sayidjarrah@lost.org' , 'Sevilla', 'Sayid', 'Jarrah', 2, 123356720, '1969-01-17', 'Sayid');
+CALL Insert_user(12345678, 'seguridad', 'shannonrutherford@lost.org' , 'Sevilla', 'Shannon', 'Rutherford', 2, 103356729, '1983-09-21', 'Shannon');
