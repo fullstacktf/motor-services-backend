9184b11a951c1e20210ad4165bcbb75d5db8e0ea
Juan Fran Martin <juanframaro@gmail.com>
2021-11-16 22:04:23 +0000

    removed -d start.sh and not allow enty root password

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 docker-compose down
 sudo rm -rf ./motor-services-backend/mysql_data
 mkdir ./motor-services-backend/mysql_data
-docker-compose build && docker-compose up -d
+docker-compose build && docker-compose up
