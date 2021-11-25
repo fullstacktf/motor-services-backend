835b0b705f08f5d2a374dfecfd657037ca4e9559
Juan Fran Martin <juanframaro@gmail.com>
2021-11-25 11:26:12 +0000

    services model

---
+++ models/service.model.js
@@ -0,0 +1,27 @@
+import { execQuery } from '../database/database.js'
+
+let data;
+let queryExec = '';
+const bodyIsEmpty = (body) => Object.keys(body).length === 0;
+
+export class ServiceModel {
+    async getServices(req, res){
+        queryExec = queryUse + 'select * from Services;';
+        data = await execQuery(queryExec);
+        res.json({
+            services: data
+        });
+    }
+    async getService(req, res){
+        const service_id = req.params.serviceID;
+        queryExec = queryUse + `select * from Services where id_service=${service_id};`;
+        data = await execQuery(queryExec);
+        if (data.length !== 0) {
+            res.json({
+                service: data
+            });
+        } else {
+            res.send("No existe ningún servicio con ese ID");
+        }
+    }
+}
