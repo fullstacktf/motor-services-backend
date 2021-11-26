40d500556130ae875a61d5f819e47170cde8994a
Juan Fran Martin <juanframaro@gmail.com>
2021-11-26 13:15:25 +0000

    added test

---
+++ tests/appointment.test.js
@@ -0,0 +1,20 @@
+import supertest from 'supertest'
+import app from '../app.js'
+import express from 'express'
+const router = express.Router()
+
+describe("Testing Appointment Endpoints",()=>{
+
+    it("Testing to see if Jest work",()=>{
+        expect(1).toBe(1)
+    })
+
+    
+    it("Should return appointments from user",()=>{
+        supertest(app)
+            .get('/appointments')
+            .expect('Content-Type', /json/)
+            .expect(200)
+        
+    })
+})
