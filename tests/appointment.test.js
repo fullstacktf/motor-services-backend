c03c5451e135c99b7d8833af28492e3c2bf38190
Juan Fran Martin <juanframaro@gmail.com>
2021-11-26 13:22:23 +0000

    added test

--- tests/appointment.test.js
+++ tests/appointment.test.js
@@ -10,7 +10,7 @@ describe("Testing Appointment Endpoints",()=>{
     })
 
     
-    it("Should return appointments from user",()=>{
+    it("Should return appointments from User",()=>{
         supertest(app)
             .get('/appointments')
             .expect('Content-Type', /json/)
