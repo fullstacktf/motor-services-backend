3d31cef335a09287aa0a3ef49d5d4bfd9314ffba
Juan Fran Martin <juanframaro@gmail.com>
2021-11-28 19:11:39 +0000

    removed async await

--- tests/vehicle.test.js
+++ tests/vehicle.test.js
@@ -1,31 +1,31 @@
 import supertest from 'supertest'
-import { app } from '../app.js'
+import {app} from '../app.js'
 import express from 'express'
 const router = express.Router()
 
 
 
-describe("Testing Vehicle Endpoints", () => {
+describe("Testing Vehicle Endpoints",()=>{
 
-    it("Testing to see if Jest work", () => {
+    it("Testing to see if Jest work",()=>{
         expect(1).toBe(1)
     })
 
-
-    it("Should return vehicles from user", async () => {
-        await supertest(app)
+    
+    it("Should return vehicles from user",()=>{
+        supertest(app)
             .get('/me/vehicles')
             .expect('Content-Type', /json/)
             .expect(200)
-
+        
     })
 
-    it("Should return one vehicle from user", async () => {
-        await supertest(app)
+    it("Should return one vehicle from user",()=>{
+        supertest(app)
             .get('/me/vehicles/:idVehicle')
             .expect('Content-Type', /json/)
             .expect(200)
-
+          
     })
 
 
