e48938436e5db6c9002ef222fae823ecbd892842
Juan Fran Martin <juanframaro@gmail.com>
2021-11-30 08:54:16 +0000

    improved database and tests

--- database/database.js
+++ database/database.js
@@ -6,8 +6,8 @@ import mariadb from 'mariadb';
 
 export const pool = 
 mariadb.createPool({
-  host: "127.0.0.1",  //mariadb en produccion
-  port: "3307",      //3306 en produccion
+  host: "127.0.0.1" || "mariadb",
+  port: "3307" || "3306", 
   user: "newuser", 
   password: "test",
   database: "pickauto",
