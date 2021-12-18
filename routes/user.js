f5f75bda4a984e9bcbf3f28426844146457939af
Marta Ramiro <martaramiroperez@gmail.com>
2021-12-18 19:18:18 +0100

    Fixed upload image endpoint

--- routes/user.js
+++ routes/user.js
@@ -19,26 +19,6 @@ router.update('/picker/:pickerID', (req,res) => {
     return userController.editPicker(req, res);
 })
 
-router.post('/upload/:userID', (req, res) => {
-    return userController.uploadImage(req, res);
-})
-
-// router.get('/:userID', (req, res) => {
-//     LoginModel.isAuthenticated(req, res, next, "user.getUser(req,res)")
-// });
-
-// router.post('/:id/auth', async (req, res) => {
-//     return user.logIn(req,res);
-// });
-
-// router.get('/logout', async (req,res) => {
-//     return user.logOut(req,res);
-// });
-
-
-// router.post('/uploadImage/:userID', uploadUtils.upload.single('dataFile'), (req, res, next) => {
-//     return userController.uploadImage(req, res, next);
-// });
-
-
-//comprobar si es picker o owner?
+router.post('/uploadImage/:userID', uploadUtils.upload.single('dataFile'), (req, res, next) => {
+    return userController.uploadImage(req, res, next);
+});
